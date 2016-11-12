5863c6a59a9bf93ef6ac992bce60b859bcc2f1f9
exhesham <exhesham@gmail.com>
2016-11-12 10:39:50 +0200

    source commit folder changes

--- src/scripts/monitor.sh
+++ src/scripts/monitor.sh
@@ -2,5 +2,5 @@
 webserver_prcces_num=$(ps aux | grep "USRMainServer.js" |grep -v grep | wc -l)
 if (( $webserver_prcces_num == 0 )); then
 	echo "web server ftw is not running...starting it."
-	nohup node "/home/pi/UnderSurvillance/USRMainServer.js" &
+	nohup node "/home/pi/UnderSurvillance/src/USRMainServer.js" &
 fi
