f5ec9efde1d763ff59f9aebb9652bf66fdda297b
exhesham <exhesham@gmail.com>
2017-01-13 23:07:14 +0200

    fix start script 2

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 
-function clear(){
+function clear_env(){
 	rm -rf /root/.forever/USR.log
 	rm -rf /root/.forever/USRMainServer.log
 	rm -rf /home/pi/UnderSurvillance/logs/*
 	forever stopall
 
 }
-function start(){
+function start_app(){
 	netstat -tlpn | grep 4330
 	if [ ! $? -eq 0 ]; then
 		forever start $SRC_DIR/USRMainServer.js -l $SRC_DIR -o $SRC_DIR -e $SRC_DIR -p $SRC_DIR
 	fi
 }
+
+clear_env
+start_app
